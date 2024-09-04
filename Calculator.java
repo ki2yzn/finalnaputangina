14a1befe9000ccf2d135d6d88769b6ef0dbd8993
ki2yzn <ki2yzn@gmail.com>
2024-09-04 23:27:10 +0800

    Add method for subtraction.

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,9 @@
+public class Calculator {
+	static int subtraction(int x, int y) {
+		return x - y;
+	}
+	public static void main(String[] args) {
+		System.out.println("Subtraction result: " + subtraction(5, 8));
+	}
+}
 
