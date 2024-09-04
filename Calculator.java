0ec8fad4dc3079f80c168205c6370015fea0d302
ki2yzn <ki2yzn@gmail.com>
2024-09-04 23:28:19 +0800

    Add method for multiplication.

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,9 @@
+public class Calculator {
+	static int multiplication(int x, int y) {
+		return x * y;
+	}
+	public static void main(String[] args) {
+		System.out.println("Multiplication result: " + multiplication(5, 8));
+	}
+}
 
