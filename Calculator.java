1e474bbaa22d0ad620c83ca0f86469da02725f77
ki2yzn <ki2yzn@gmail.com>
2024-09-04 23:26:09 +0800

    Add method for addition.

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,8 @@
-
+public class Calculator {
+	static int addition(int x, int y) {
+		return x + y;
+	}
+	public static void main(String[] args) {
+		System.out.println("Addition result: " + addition(5, 8));
+	}
+}
