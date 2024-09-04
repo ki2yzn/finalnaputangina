c0605118943c9e7fbf32b9b1faabf326ab9d5227
ki2yzn <ki2yzn@gmail.com>
2024-09-04 23:38:56 +0800

    Add method for division.

--- Calculator.java
+++ Calculator.java
@@ -10,10 +10,15 @@ public class Calculator {
 	static int subtraction(int x, int y) {
 		return x - y;
 	}
+
+	static int division(int x, int y) {
+		return x / y;
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Subtraction result: " + subtraction(5, 8));
 		System.out.println("Addition result: " + addition(5, 8));
 		System.out.println("Multiplication result: " + multiplication(5, 8));
+		System.out.println("Division result: " + division(5, 8));
 	}
 }
